beba80e86ce51ee09a9ed15effbf6c30234e5c28
Anudeep Debbata <30561588+Anudepp@users.noreply.github.com>
2024-11-01 12:15:12 -0500

    Cosmetic/RestaurentCards-Styles

--- src/components/Resturentcard.js
+++ src/components/Resturentcard.js
@@ -3,21 +3,23 @@ import { SWIGGY_CDN_URL } from "../utils/constants";
 const Resturentcard = (props) => {
   const { resData } = props;
   const { cloudinaryImageId, name, avgRating, cuisines, costForTwo, sla } = resData?.info;
-  
 
-  
   return (
-    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+    <div className="res-card">
       <img 
         src={`${SWIGGY_CDN_URL}${cloudinaryImageId}`}
         alt={`${name} image`}
         className="res-logo"
       />
-      <h3>{name}</h3>
-      <h3>{cuisines.join(", ")}</h3> 
-      <h3>{avgRating} <span className="star-symbol">⋆</span></h3>
-      <h3>{costForTwo}</h3>
-      <h3>{sla.deliveryTime} minutes</h3>
+      <div className="res-card-content">
+        <h3 className="res-name">{name}</h3>
+        <p className="res-cuisines">{cuisines.join(", ")}</p>
+        <div className="res-rating">
+          <span>{avgRating}</span> <span className="star-symbol">⋆</span>
+        </div>
+        <p className="res-cost">{costForTwo}</p>
+        <p className="res-delivery-time">{sla.deliveryTime} min</p>
+      </div>
     </div>
   );
 };
