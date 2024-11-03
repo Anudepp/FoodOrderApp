ecdb31b4da6090e5902ba4e89277e1016627b71d
Anudeep Debbata <30561588+Anudepp@users.noreply.github.com>
2024-11-03 17:31:01 -0600

    feature/GitHubApiCall

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -3,37 +3,62 @@ import React from 'react';
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
-    console.log(this.props.name +" Child Constructor");
 
     // Multiple Local state variable
     this.state = {
-      count: 1,
-      count2: 2
+      userInfo: {
+        name: "",
+        location: "",
+        contact: "",
+        bio: "",
+      }
     };
   };
   
-  componentDidMount() {
-    console.log(this.props.name +" Child componentDidMount");
+
+  // componentDidMount is used to make API calls, fetch data from the server, etc.
+  async componentDidMount() {
+    const data = await fetch("https://api.github.com/users/Anudepp");
+    const json = await data.json();
+
+    console.log(json);
+
+    // Update the state with the fetched data
+    this.setState({
+      userInfo: {
+        name: json.name,
+        location: json.location,
+        contact: json.email,
+        bio: json.bio,
+      }
+    });
   }
+
+  // componentDidUpdate is used to update the DOM when the state changes
+  componentDidUpdate() { 
+    console.log("componentDidUpdate");
+  }
+  
+  // componentWillUnmount is used to clean up the resources
+  componentWillUnmount() { 
+    console.log("componentWillUnmount");
+  }
+
+
   render() {
     
     // Array destructuring
-    const { name, location, contact } = this.props;
-    const { count } = this.state;
-
-    console.log(this.props.name +" Child Render");
+    const { name, location, contact,bio } = this.state.userInfo;
+    
 
 
+    // Display the fetched data
     return (
       <div className="user-card">
         <h2>{name}</h2>
         <h3>{location}</h3>
         <h3>{contact}</h3>
-        <h3>Count = {count} </h3> 
-        <button onClick={() => {
-          // Update the state variable count by 1
-          this.setState({ count: count + 1 });
-         }}>increase count</button>
+        <h3>{ bio}</h3>
       </div>
     );
   }
